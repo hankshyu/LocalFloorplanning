ceb6aae346ec8447f2321b4fcfa485e267f20dc8
hankshyu <hankshyu@gmail.com>
2023-09-02 17:16:22 +0800

    don modify deep copy of lfunits

--- src/main.cpp
+++ src/main.cpp
@@ -16,119 +16,120 @@
 
 int main(int argc, char const *argv[]) {
 
-    RGParser rgparser(argv[1]);
-    RGSolver solver;
-    solver.readFromParser(rgparser);
+
+    // RGParser rgparser(argv[1]);
+    // RGSolver solver;
+    // solver.readFromParser(rgparser);
     
-    mnt::Monitor monitor;
-    monitor.printCopyRight();
+    // mnt::Monitor monitor;
+    // monitor.printCopyRight();
     
-    /* Phase 1: Global Floorplanning */
-    std::cout << std::endl << std::endl;
-    monitor.printPhase("Global Floorplanning Phase");
-    auto clockCounterbegin = std::chrono::steady_clock::now();
+    // /* Phase 1: Global Floorplanning */
+    // std::cout << std::endl << std::endl;
+    // monitor.printPhase("Global Floorplanning Phase");
+    // auto clockCounterbegin = std::chrono::steady_clock::now();
 
-    // Parser parser(argv[1]);
-    int pushForceList[8] = { 5, 10, 15, 20, 25, 30, 40, 50 };
-    int pushScale = 0;
+    // // Parser parser(argv[1]);
+    // int pushForceList[8] = { 5, 10, 15, 20, 25, 30, 40, 50 };
+    // int pushScale = 0;
 
-    LFLegaliser *legaliser;
-    int iteration = 6000;
-    double lr = 1. / iteration;
+    // LFLegaliser *legaliser;
+    // int iteration = 6000;
+    // double lr = 1. / iteration;
     
 
-    // ! These parameters can be modified to meet your needs
-    solver.setPunishment(0.03);
-    double tolaranceLen = ( rgparser.getDieWidth() + rgparser.getDieHeight() ) / 200;
+    // // ! These parameters can be modified to meet your needs
+    // solver.setPunishment(0.03);
+    // double tolaranceLen = ( rgparser.getDieWidth() + rgparser.getDieHeight() ) / 200;
 
-    for ( int phase = 1; phase <= 50; phase++ ) {
-        solver.setSizeScalar(phase * 0.02);
-        solver.setOverlapTolaranceLen(tolaranceLen * phase * 0.02);
-        for ( int i = 0; i < iteration; i++ ) {
-            solver.calcGradient();
-            solver.gradientDescent(lr);
-        }
-    }
+    // for ( int phase = 1; phase <= 50; phase++ ) {
+    //     solver.setSizeScalar(phase * 0.02);
+    //     solver.setOverlapTolaranceLen(tolaranceLen * phase * 0.02);
+    //     for ( int i = 0; i < iteration; i++ ) {
+    //         solver.calcGradient();
+    //         solver.gradientDescent(lr);
+    //     }
+    // }
 
-    solver.currentPosition2txt("outputs/global_test.txt");
-    std::cout << std::fixed;
-    std::cout << "Estimated HPWL: " << std::setprecision(2) << solver.calcEstimatedHPWL() << std::endl;
+    // solver.currentPosition2txt("outputs/global_test.txt");
+    // std::cout << std::fixed;
+    // std::cout << "Estimated HPWL: " << std::setprecision(2) << solver.calcEstimatedHPWL() << std::endl;
 
-    legaliser = new LFLegaliser((len_t) rgparser.getDieWidth(), (len_t) rgparser.getDieHeight());
-    legaliser->translateGlobalFloorplanning(solver);
-    legaliser->detectfloorplanningOverlaps();
+    // legaliser = new LFLegaliser((len_t) rgparser.getDieWidth(), (len_t) rgparser.getDieHeight());
+    // legaliser->translateGlobalFloorplanning(solver);
+    // legaliser->detectfloorplanningOverlaps();
 
-    // Phase 1 Reports
-    std::cout << std::endl;
-    monitor.printPhaseReport();
-    // std::cout << "Estimated HPWL in Global Phase: " << std::setprecision(2) << solver->calcEstimatedHPWL() << std::endl;
-    std::cout << "Multiple Tile overlap (>3) count: " << legaliser->has3overlap() << std::endl;
-    // solver->currentPosition2txt("outputs/ppmoduleResult.txt");
-    legaliser->visualiseArtpiece("outputs/phase1.txt", false);
+    // // Phase 1 Reports
+    // std::cout << std::endl;
+    // monitor.printPhaseReport();
+    // // std::cout << "Estimated HPWL in Global Phase: " << std::setprecision(2) << solver->calcEstimatedHPWL() << std::endl;
+    // std::cout << "Multiple Tile overlap (>3) count: " << legaliser->has3overlap() << std::endl;
+    // // solver->currentPosition2txt("outputs/ppmoduleResult.txt");
+    // legaliser->visualiseArtpiece("outputs/phase1.txt", false);
     
     
-    /* Phase 2: Processing Corner Stiching */
-    std::cout << std::endl << std::endl;
-    monitor.printPhase("Extracting geographical information to IR");
+    // /* Phase 2: Processing Corner Stiching */
+    // std::cout << std::endl << std::endl;
+    // monitor.printPhase("Extracting geographical information to IR");
 
 
-    std::cout << "2.1 Performing split ...";
-    legaliser->splitTesseraeOverlaps();
-    std::cout << "done!" << std::endl;
+    // std::cout << "2.1 Performing split ...";
+    // legaliser->splitTesseraeOverlaps();
+    // std::cout << "done!" << std::endl;
 
-    std::cout << "2.2 Painting All Tesserae to Canvas" << std::endl;
-    legaliser->arrangeTesseraetoCanvas();
+    // std::cout << "2.2 Painting All Tesserae to Canvas" << std::endl;
+    // legaliser->arrangeTesseraetoCanvas();
     
-    std::cout << "2.3 Start combinable tile search, ";
-    std::vector <std::pair<Tile *, Tile *>> detectMergeTile;
-    legaliser->detectCombinableBlanks(detectMergeTile);
-    std::cout << detectMergeTile.size() << " candidates found" << std::endl << std::endl;
-    for(std::pair<Tile *, Tile *> tp : detectMergeTile){
-        legaliser->visualiseAddMark(tp.first);
-        legaliser->visualiseAddMark(tp.second);
-    }
-    legaliser->visualiseArtpiece("outputs/phase2_1.txt", true);
+    // std::cout << "2.3 Start combinable tile search, ";
+    // std::vector <std::pair<Tile *, Tile *>> detectMergeTile;
+    // legaliser->detectCombinableBlanks(detectMergeTile);
+    // std::cout << detectMergeTile.size() << " candidates found" << std::endl << std::endl;
+    // for(std::pair<Tile *, Tile *> tp : detectMergeTile){
+    //     legaliser->visualiseAddMark(tp.first);
+    //     legaliser->visualiseAddMark(tp.second);
+    // }
+    // legaliser->visualiseArtpiece("outputs/phase2_1.txt", true);
 
-    legaliser->visualiseRemoveAllmark();
-    while(!detectMergeTile.empty()){
-        std::cout << "Merging Pair: #" << detectMergeTile.size() << std::endl;
-        detectMergeTile[0].first->show(std::cout);
-        detectMergeTile[0].second->show(std::cout);
+    // legaliser->visualiseRemoveAllmark();
+    // while(!detectMergeTile.empty()){
+    //     std::cout << "Merging Pair: #" << detectMergeTile.size() << std::endl;
+    //     detectMergeTile[0].first->show(std::cout);
+    //     detectMergeTile[0].second->show(std::cout);
 
-        legaliser->combineVerticalMergeableBlanks(detectMergeTile[0].first, detectMergeTile[0].second);
-        detectMergeTile.clear();
-        legaliser->detectCombinableBlanks(detectMergeTile);
-    }
+    //     legaliser->combineVerticalMergeableBlanks(detectMergeTile[0].first, detectMergeTile[0].second);
+    //     detectMergeTile.clear();
+    //     legaliser->detectCombinableBlanks(detectMergeTile);
+    // }
 
 
-    // Phase 2 Reports
-    std::cout << std::endl;
-    monitor.printPhaseReport();
+    // // Phase 2 Reports
+    // std::cout << std::endl;
+    // monitor.printPhaseReport();
     
-    legaliser->visualiseArtpiece("outputs/phase2.txt", true);
+    // legaliser->visualiseArtpiece("outputs/phase2.txt", true);
 
 
-    /* Phase 3: Promitive Overlap Removal */
-    std::cout << std::endl << std::endl;
-    monitor.printPhase("Primitive removal/break-down of Overlaps");
+    // /* Phase 3: Promitive Overlap Removal */
+    // std::cout << std::endl << std::endl;
+    // monitor.printPhase("Primitive removal/break-down of Overlaps");
 
-    std::vector <RGConnStruct> connectionList = rgparser.getConnectionList();
-    paletteKnife spatula(legaliser, &connectionList);
+    // std::vector <RGConnStruct> connectionList = rgparser.getConnectionList();
+    // paletteKnife spatula(legaliser, &connectionList);
     
-    spatula.disperseViaMargin();
+    // spatula.disperseViaMargin();
     
-    // Phase 3 Reports
-    std::cout << std::endl;
-    std::cout << "Overlap Report: " << std::endl;
-    spatula.collectOverlaps();
-    spatula.printpaintClusters();
-    monitor.printPhaseReport();
-    legaliser->visualiseArtpiece("outputs/phase3.txt", true);
+    // // Phase 3 Reports
+    // std::cout << std::endl;
+    // std::cout << "Overlap Report: " << std::endl;
+    // spatula.collectOverlaps();
+    // spatula.printpaintClusters();
+    // monitor.printPhaseReport();
+    // legaliser->visualiseArtpiece("outputs/phase3.txt", true);
 
-    /* Phase 4: Solve 2-Overlaps */
-    std::cout << std::endl << std::endl;
-    monitor.printPhase("Solve Level-2 Overlaps");
-    spatula.eatCakesLevel2();
+    // /* Phase 4: Solve 2-Overlaps */
+    // std::cout << std::endl << std::endl;
+    // monitor.printPhase("Solve Level-2 Overlaps");
+    // spatula.eatCakesLevel2();
 
 
 
